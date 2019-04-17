63611feec7049e622262e013df6f686c40f97fec
Timothy Yuan <timothyyuan@cs.ucsb.edu>
2019-04-16 19:55:46 -0700

    studentRoll

--- studentRoll.cpp
+++ studentRoll.cpp
@@ -6,20 +6,44 @@ StudentRoll::StudentRoll() {
 }
 
 void StudentRoll::insertAtTail(const Student &s) {
-  // STUB
+	Node *n = new Node{new Student(s), NULL};
+	if(!head){
+		head = n;
+		tail = n;
+	}else{
+		tail->next = n;
+		tail = n;
+	}
 }
 
 std::string StudentRoll::toString() const {
-  return "stub";
+  	std::string s = "[";
+	Node *n = this->head;
+	while(n){
+		s+=n->s->toString();
+		if(n->next)s+=",";
+		n=n->next;
+	}
+	s+="]";
+	return s;
 }
 
 StudentRoll::StudentRoll(const StudentRoll &orig) {
-  // STUB
   head = tail = NULL;
+  Node *n = orig.head;
+  while(n){
+  	this->insertAtTail(*(n->s));
+	n = n->next;
+  }
 }
 
 StudentRoll::~StudentRoll() {
-  // STUB
+  Node *n = this->head;
+  while(n){
+  	Node *tmp = n;
+	n = n->next;
+	delete tmp;
+  }
 }
 
 StudentRoll & StudentRoll::operator =(const StudentRoll &right ) {
@@ -33,7 +57,18 @@ StudentRoll & StudentRoll::operator =(const StudentRoll &right ) {
   // TODO... Here is where there is code missing that you need to 
   // fill in...
 
-
+  Node *n = this->head;
+  while(n){
+  	Node *tmp = n;
+	n = n->next;
+	delete tmp;
+  }
+  head = tail = NULL;
+  n = right.head;
+  while(n){
+  	this->insertAtTail(*(n->s));
+	n = n->next;
+  }
   // KEEP THE CODE BELOW THIS LINE
   // Overloaded = should end with this line, despite what the textbook says.
   return (*this); 
